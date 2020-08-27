be76d898fd753afd15441df9aead774c11a557b9
QDSG <piftao1127@163.com>
2020-08-27 13:43:50 +0800

    init

---
+++ Project-5/WordScramble/WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by QDSG on 2020/8/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
