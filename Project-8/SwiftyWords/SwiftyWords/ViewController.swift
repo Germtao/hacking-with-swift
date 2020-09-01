9a32ae4be8b8f0235fbd3fe5ed3a0a2f63eed144
QDSG <piftao1127@163.com>
2020-09-01 15:23:32 +0800

    init

---
+++ Project-8/SwiftyWords/SwiftyWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyWords
+//
+//  Created by QDSG on 2020/9/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
