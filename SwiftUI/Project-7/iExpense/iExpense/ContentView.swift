9d0d69a05ddacc419683815778f0d2e314f8b3f9
QDSG <piftao1127@163.com>
2020-09-21 17:33:36 +0800

    iExpense

---
+++ SwiftUI/Project-7/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by QDSG on 2020/6/30.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // @ObservedObject 监听这些公告，并使用该对象刷新所有视图
+    @ObservedObject var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        Text("$\(item.amount)")
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(
+                trailing: Button(action: {
+                    self.showingAddExpense = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: self.expenses)
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Project-7/iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,21 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by QDSG on 2020/6/30.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import Foundation
+
+// Identifiable: Swift内置的协议之一，表示“可以唯一地标识此类型”
+// 它只有一个要求，那就是必须有一个名为id的属性，其中包含唯一的标识符
+
+// Codable协议，这将使我们能够存档所有准备存储的现有费用项目
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID() // 唯一的标志
+    let name: String
+    let type: String
+    let amount: Int
+}
