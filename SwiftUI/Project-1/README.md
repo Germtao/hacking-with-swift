75f586762d6513ad24e4c6896b0a3ecf5b0cca90
QDSG <piftao1127@163.com>
2020-09-21 10:00:30 +0800

    WeSplit

---
+++ SwiftUI/Project-1/README.md
@@ -0,0 +1,11 @@
+#  SwiftUI 
+
+> Form、NavigationView、 @State等内容。
+
+- 了解`SwiftUI`应用的基本结构
+- 创建表格：`Form`
+- 添加导航栏：`NavigationView`
+- 修改程序状态
+- 将状态绑定到用户界面控件：`@State`
+- 循环创建视图
+
---
+++ SwiftUI/Project-1/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by QDSG on 2020/5/6.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    /// 账单金额
+    @State private var checkAmount = ""
+    /// 人数
+    @State private var numberOfPeople = 2
+    /// 小费百分比
+    @State private var tipPercentage = 2
+    
+    /// 小费百分比列表
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    /// 同步的原因：
+    /// 1. 我们的文本输入框与checkAmount属性有双向绑定
+    /// 2. checkAmount属性用@State标记，它自动监视值的更改
+    /// 3. 当@State属性更改时，SwiftUI将重新调用body属性（即，重新加载我们的UI）
+    /// 4. 因此，文本视图将获得checkAmount的更新值
+    
+    var totalPerPerson: Double {
+        // 在这里计算每人的总数
+        let peopleCount = Double(numberOfPeople)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("账单金额", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("人数", selection: $numberOfPeople) {
+                        ForEach(0 ..< 100) {
+                            Text("\($0)人")
+                        }
+                    }
+                }
+                
+                Section {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("离开时，你想付多少小费？")) {
+                    Picker("小费百分比", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+            .navigationBarTitle("账单")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
