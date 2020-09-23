d74946b22231f07b006351abab43897024ad140d
QDSG <piftao1127@163.com>
2020-09-23 17:50:27 +0800

    创建NSManagedObject子类

---
+++ SwiftUI/Project-12/CoreDataDemo/CoreDataDemo/Extension/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataDemo
+//
+//  Created by QDSG on 2020/9/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
