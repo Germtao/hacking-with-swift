6b9c578c38b039bf5b3e9f15bc4cf778a8e22431
QDSG <piftao1127@163.com>
2020-09-23 14:12:33 +0800

    使用@FetchRequest建立List

--- SwiftUI/Project-11/Bookworm/Bookworm/ContentView.swift
+++ SwiftUI/Project-11/Bookworm/Bookworm/ContentView.swift
@@ -11,6 +11,7 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var moc
 
+    /// 使用@FetchRequest建立 list
     @FetchRequest(
         entity: Book.entity(),
         sortDescriptors: [
@@ -23,14 +24,31 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            Text("数量: \(books.count)")
-                .navigationBarTitle("书架")
-                .navigationBarItems(trailing: Button(action: { showingAddScreen.toggle() }, label: {
-                    Image(systemName: "plus")
-                }))
-                .sheet(isPresented: $showingAddScreen, content: {
-                    AddBookView().environment(\.managedObjectContext, moc)
-                })
+            List {
+                ForEach(books, id: \.self) { book in
+                    NavigationLink(
+                        destination: Text(book.title ?? "未知"),
+                        label: {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "未知")
+                                    .font(.headline)
+                                Text(book.author ?? "未知")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    )
+                }
+            }
+            .navigationBarTitle("书架")
+            .navigationBarItems(trailing: Button(action: { showingAddScreen.toggle() }, label: {
+                Image(systemName: "plus")
+            }))
+            .sheet(isPresented: $showingAddScreen, content: {
+                AddBookView().environment(\.managedObjectContext, moc)
+            })
         }
     }
 }
