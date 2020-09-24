0176a839314a7ebefa00e4bb8cbc3b5189dd68ee
QDSG <piftao1127@163.com>
2020-09-24 14:43:42 +0800

    使用NSPredicate过滤@FetchRequest

--- SwiftUI/Project-12/CoreDataDemo/CoreDataDemo/ContentView.swift
+++ SwiftUI/Project-12/CoreDataDemo/CoreDataDemo/ContentView.swift
@@ -12,37 +12,41 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var moc
 
     @FetchRequest(
-        entity: Wizard.entity(),
-        sortDescriptors: []
-    ) var wizards: FetchedResults<Wizard>
+        entity: Ship.entity(),
+        sortDescriptors: [],
+        predicate: NSPredicate(format: "universe == 'Star Wars'")
+        // or NSPredicate(format: "universe == %@", "Star Wars")
+        // or NSPredicate(format: "name < %@", "F")
+        // NSPredicate(format: "universe IN %@", ["Aliens", "Firefly", "Star Trek"])
+        // NSPredicate(format: "name BEGINSWITH %@", "E")
+        // NSPredicate(format: "name BEGINSWITH[c] %@", "e") 忽略大小写
+        // NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")
+    ) var ships: FetchedResults<Ship>
 
     var body: some View {
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
             }
             
-            Button("添加") {
-                let wizard = Wizard(context: moc)
-                wizard.name = "Harry Potter"
-            }
-            
-            Button("保存") {
-                withAnimation {
-                    do {
-                        try moc.save()
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            
-            Button("删除") {
-                wizards.forEach {
-                    moc.delete($0)
-                }
-                
-                try? moc.save()
+            Button("添加例子") {
+                let ship1 = Ship(context: self.moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+
+                let ship2 = Ship(context: self.moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+
+                let ship3 = Ship(context: self.moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+
+                let ship4 = Ship(context: self.moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+
+                try? self.moc.save()
             }
         }
     }
