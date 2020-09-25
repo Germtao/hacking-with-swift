5d337c7d6936bc89f8bfe0f28caa88a6b8bce14a
QDSG <piftao1127@163.com>
2020-09-25 15:05:36 +0800

    使用ActionSheet显示多个选项

--- SwiftUI/ViewsAndViewControllers/Project-13/Instafilter/Instafilter/ContentView.swift
+++ SwiftUI/ViewsAndViewControllers/Project-13/Instafilter/Instafilter/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var blurAmount: CGFloat = 0
     
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
+    
     var body: some View {
         let blur = Binding<CGFloat> {
             self.blurAmount
@@ -20,7 +23,30 @@ struct ContentView: View {
         
         return VStack {
             Text("Hello, world!")
+                .frame(width: 300, height: 300)
+                .background(backgroundColor)
                 .blur(radius: blurAmount)
+                .onTapGesture {
+                    showingActionSheet = true
+                }
+                .actionSheet(isPresented: $showingActionSheet, content: {
+                    ActionSheet(
+                        title: Text("改变颜色"),
+                        message: Text("选择一个新的颜色"),
+                        buttons: [
+                            .default(Text("红色"), action: {
+                                backgroundColor = .red
+                            }),
+                            .default(Text("绿色"), action: {
+                                backgroundColor = .green
+                            }),
+                            .default(Text("蓝色"), action: {
+                                backgroundColor = .blue
+                            }),
+                            .cancel(Text("取消"))
+                        ]
+                    )
+                })
             
             Slider(value: blur, in: 0...20)
         }
