7e07dd7d295ffb1261c6e74cc74816fff6125701
QDSG <piftao1127@163.com>
2020-09-27 16:25:32 +0800

    将数据写入文档目录

--- SwiftUI/ViewsAndViewControllers/Project-14/BucketList/BucketList/ContentView.swift
+++ SwiftUI/ViewsAndViewControllers/Project-14/BucketList/BucketList/ContentView.swift
@@ -19,9 +19,31 @@ struct ContentView: View {
 //    }
     
     var body: some View {
-        List(users) { user in
-            Text("\(user.firstName) - \(user.lastName)")
-        }
+        Text("Hello World")
+            .onTapGesture {
+                let str = "Test Message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print("input: \(input)")
+                } catch {
+                    print("写文件发生错误: \(error.localizedDescription)")
+                }
+            }
+    }
+}
+
+extension ContentView {
+    private func getDocumentsDirectory() -> URL {
+        // 查找该用户的所有可能的文档目录
+        let path = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask
+        )
+        // 只需发回第一个，应该是唯一的一个
+        return path[0]
     }
 }
 
