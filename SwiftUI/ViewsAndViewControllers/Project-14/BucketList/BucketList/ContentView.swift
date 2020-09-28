bdb8d5d0787822f4397ae8958c9a8b3cc668982b
QDSG <piftao1127@163.com>
2020-09-28 12:09:20 +0800

    在SwiftUI中使用Touch ID和Face ID

--- SwiftUI/ViewsAndViewControllers/Project-14/BucketList/BucketList/ContentView.swift
+++ SwiftUI/ViewsAndViewControllers/Project-14/BucketList/BucketList/ContentView.swift
@@ -6,18 +6,49 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
     
-    var loadingState = LoadingState.loading
+    @State private var isUnlocked = false
     
     var body: some View {
-        MapView()
-            .edgesIgnoringSafeArea(.all)
+        VStack {
+            if self.isUnlocked {
+                Text("已解锁")
+            } else {
+                Text("已锁定")
+            }
+        }
+        .onAppear(perform: authenticate)
     }
 }
 
 extension ContentView {
+    /// FaceID认证
+    private func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        // 检查生物识别认证是否可用
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "我们需要解锁您的数据"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { (success, error) in
+                // 身份验证已完成
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        print("认证失败: \(error?.localizedDescription ?? "Unknown error.")")
+                    }
+                }
+            }
+        } else {
+            print("没有生物识别功能")
+        }
+    }
+    
     private func getDocumentsDirectory() -> URL {
         // 查找该用户的所有可能的文档目录
         let path = FileManager.default.urls(
