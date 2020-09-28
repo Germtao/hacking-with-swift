2f0e48d5a120dfb3ef68b2517ec55369c5b9bccc
QDSG <piftao1127@163.com>
2020-09-28 10:41:19 +0800

    与MapKit协调器通信

--- SwiftUI/ViewsAndViewControllers/Project-14/BucketList/BucketList/Views/MapView.swift
+++ SwiftUI/ViewsAndViewControllers/Project-14/BucketList/BucketList/Views/MapView.swift
@@ -13,11 +13,41 @@ struct MapView: UIViewRepresentable {
     
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "杭州"
+        annotation.subtitle = "浙江省会城市"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 30.16, longitude: 120.12)
+        mapView.addAnnotation(annotation)
+        
         return mapView
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
         
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        let parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
     }
 }
 
