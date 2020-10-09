ad0028aed1512cb143145fe39942df7115dd52b2
QDSG <piftao1127@163.com>
2020-10-09 10:15:50 +0800

    init

---
+++ SwiftUI/ControllingUIFlow/Project-16/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by QDSG on 2020/10/9.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
