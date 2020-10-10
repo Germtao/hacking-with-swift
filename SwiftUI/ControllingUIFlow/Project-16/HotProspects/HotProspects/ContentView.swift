f12caca2d686f64b7f98394996185d0326f98980
QDSG <piftao1127@163.com>
2020-10-10 10:54:20 +0800

    创建上下文菜单

--- SwiftUI/ControllingUIFlow/Project-16/HotProspects/HotProspects/ContentView.swift
+++ SwiftUI/ControllingUIFlow/Project-16/HotProspects/HotProspects/ContentView.swift
@@ -14,15 +14,66 @@ enum NetworkError: Error {
 struct ContentView: View {
     @ObservedObject var updater = DelayedUpdater()
     
+    @State private var backgroundColor = Color.red
+    @State private var selectedTag = 0
+    
     var body: some View {
-        Image("example")
-            .interpolation(.none)
-            .resizable()
-            .scaledToFit()
-            .frame(maxHeight: .infinity)
-            .background(Color.black)
-            .edgesIgnoringSafeArea(.all)
+        VStack {
+            Text("Hello World!")
+                .padding()
+                .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+                .contextMenu(menuItems: /*@START_MENU_TOKEN@*/{
+                    Button(action: {
+                        self.backgroundColor = .red
+                        self.selectedTag = 0
+                    }, label: {
+                        Text("Red")
+                        if self.selectedTag == 0 {
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(.red)
+                        }
+                    })
+                    .tag(0)
+                    
+                    Button(action: {
+                        self.backgroundColor = .green
+                        self.selectedTag = 1
+                    }, label: {
+                        Text("Green")
+                        if self.selectedTag == 1 {
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(.red)
+                        }
+                    })
+                    .tag(1)
+                    
+                    Button(action: {
+                        self.backgroundColor = .blue
+                        self.selectedTag = 2
+                    }, label: {
+                        Text("Blue")
+                        if self.selectedTag == 2 {
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(.red)
+                        }
+                    })
+                    .tag(2)
+                }/*@END_MENU_TOKEN@*/)
+        }
     }
+    
+//    var body: some View {
+//        Image("example")
+//            .interpolation(.none)
+//            .resizable()
+//            .scaledToFit()
+//            .frame(maxHeight: .infinity)
+//            .background(Color.black)
+//            .edgesIgnoringSafeArea(.all)
+//    }
     
 //    var body: some View {
 //        Text("value is: \(updater.value)")
