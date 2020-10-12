99225d3e22f9c48dfd043ca88df78e4e54e1df74
QDSG <piftao1127@163.com>
2020-10-12 10:05:04 +0800

    生成并扩大QR码

--- SwiftUI/ControllingUIFlow/Project-16/HotProspects/HotProspects/Views/MeView.swift
+++ SwiftUI/ControllingUIFlow/Project-16/HotProspects/HotProspects/Views/MeView.swift
@@ -28,10 +28,31 @@ struct MeView: View {
                     .font(.title)
                     .padding([.horizontal, .bottom])
                 
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
                 Spacer()
             }
             .navigationBarTitle("Your code")
         }
+    }
+}
+
+extension MeView {
+    private func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgimage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimage)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
