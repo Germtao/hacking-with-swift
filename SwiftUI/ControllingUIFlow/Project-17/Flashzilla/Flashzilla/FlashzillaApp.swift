ab648efbec80f1fa505ce34e2d8f1f8bf5bc4d62
QDSG <piftao1127@163.com>
2020-10-12 14:49:46 +0800

    init

---
+++ SwiftUI/ControllingUIFlow/Project-17/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by QDSG on 2020/10/12.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
