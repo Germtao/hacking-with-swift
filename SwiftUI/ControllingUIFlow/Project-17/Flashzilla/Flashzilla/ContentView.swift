4aa4e19e9f23d0837fe940490e9f5ea38d1aee75
QDSG <piftao1127@163.com>
2020-10-13 11:07:30 +0800

    通过SwiftUI支持特定的可访问性需求

--- SwiftUI/ControllingUIFlow/Project-17/Flashzilla/Flashzilla/ContentView.swift
+++ SwiftUI/ControllingUIFlow/Project-17/Flashzilla/Flashzilla/ContentView.swift
@@ -9,17 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    
+//    @Environment(\.accessibilityReduceMotion) var reduceMotion
+//    @State private var scale: CGFloat = 1
+    
+//    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
     var body: some View {
         Text("Hello, World!")
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification), perform: { _ in
-                print("Moving to the background!")
-            })
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification), perform: { _ in
-                print("Moving back to the foreground!")
-            })
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification), perform: { _ in
-                print("User took a screenshot!")
-            })
+            .padding()
+            .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+        
+//        Text("Hello, World!")
+//            .scaleEffect(scale)
+//            .onTapGesture(count: 1, perform: {
+//                if self.reduceMotion {
+//                    self.scale *= 1.5
+//                } else {
+//                    withAnimation {
+//                        self.scale *= 1.5
+//                    }
+//                }
+//                withOptionalAnimation {
+//                    self.scale *= 1.5
+//                }
+//            })
+        
+//        HStack {
+//            if differentiateWithoutColor {
+//                Image(systemName: "checkmark.circle")
+//            }
+//
+//            Text("Success")
+//        }
+//        .padding()
+//        .background(differentiateWithoutColor ? Color.black : Color.green)
+//        .foregroundColor(.white)
+//        .clipShape(Capsule())
+    }
+}
+
+extension ContentView {
+    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+        if UIAccessibility.isReduceMotionEnabled {
+            return try body()
+        } else {
+            return try withOptionalAnimation(animation, body)
+        }
     }
 }
 
