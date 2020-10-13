147b7daea79bf35eb8f670ecdb478ffa6bde666d
QDSG <piftao1127@163.com>
2020-10-13 15:37:05 +0800

    滑动时改变视图颜色

--- SwiftUI/ControllingUIFlow/Project-17/Flashzilla/Flashzilla/Views/CardView.swift
+++ SwiftUI/ControllingUIFlow/Project-17/Flashzilla/Flashzilla/Views/CardView.swift
@@ -12,13 +12,27 @@ struct CardView: View {
     
     var removal: (() -> Void)? = nil
     
+    /// 无色差异 (红绿色盲)
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.white)
+                .fill(
+                    differentiateWithoutColor
+                        ? Color.white
+                        : Color.white
+                            .opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background(
+                    differentiateWithoutColor
+                        ? nil
+                        : RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                            .fill(offset.width > 0 ? Color.green : Color.red)
+                )
                 .shadow(radius: 10)
             
             VStack {
