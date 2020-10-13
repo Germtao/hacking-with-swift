0db2f8ac1a2c4eefc361196e0a60d5d399f6d578
QDSG <piftao1127@163.com>
2020-10-13 10:14:51 +0800

    使用计时器重复触发事件

--- SwiftUI/ControllingUIFlow/Project-17/Flashzilla/Flashzilla/ContentView.swift
+++ SwiftUI/ControllingUIFlow/Project-17/Flashzilla/Flashzilla/ContentView.swift
@@ -6,102 +6,24 @@
 //
 
 import SwiftUI
-import CoreHaptics
 
 struct ContentView: View {
+//    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
     
-    @State private var engine: CHHapticEngine?
+    @State private var counter = 0
     
     var body: some View {
-        
-        VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("World")
-        }
-        .contentShape(Rectangle()) // Spacer区域也能点击
-        .onTapGesture(count: 1, perform: {
-            print("VStack Tapped.")
-        })
-        
-//        ZStack {
-//            Rectangle()
-//                .fill(Color.blue)
-//                .frame(width: 300, height: 300)
-//                .onTapGesture {
-//                    print("Rectangle tapped!")
-//                }
-//
-//            Circle()
-//                .fill(Color.red)
-//                .frame(width: 300, height: 300)
-//                .contentShape(Rectangle()) // 2. 指定可点击的形状
-//                .onTapGesture {
-//                    print("Circle tapped!")
-//                }
-////                .allowsHitTesting(false) // 1. 不可点击
-//        }
-        
-//        Text("Hello, world!")
-//            .onAppear(perform: prepareHaptics)
-//            .onTapGesture(perform: complexSuccess)
-        
-//            .onTapGesture(perform: simpleSuccess)
-    }
-}
-
-extension ContentView {
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.error)
-    }
-    
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        do {
-            self.engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
-        }
-    }
-    
-    func complexSuccess() {
-        // 确保设备支持触觉
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        var events = [CHHapticEvent]()
-        
-        // 创建一个强烈而尖锐的Tap
-//        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-//        events.append(event)
-        
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-        
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-        
-        // 将这些事件转换为模式并立即播放
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
-
-        }
+        Text("Hello, World!")
+            .onReceive(timer, perform: { time in
+                if self.counter == 5 {
+                    self.timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
+                }
+                
+                self.counter += 1
+            })
     }
 }
 
