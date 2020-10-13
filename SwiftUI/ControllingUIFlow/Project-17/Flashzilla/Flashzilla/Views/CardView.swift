1ca8d90c7f3357f3c4f2d97f1b6aedf3d5461839
QDSG <piftao1127@163.com>
2020-10-13 17:29:28 +0800

    使用UINotificationFeedbackGenerator使iPhone振动

--- SwiftUI/ControllingUIFlow/Project-17/Flashzilla/Flashzilla/Views/CardView.swift
+++ SwiftUI/ControllingUIFlow/Project-17/Flashzilla/Flashzilla/Views/CardView.swift
@@ -18,6 +18,8 @@ struct CardView: View {
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
     
+    @State private var feedback = UINotificationFeedbackGenerator()
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
@@ -57,9 +59,15 @@ struct CardView: View {
             DragGesture()
                 .onChanged { gesture in
                     self.offset = gesture.translation
+                    self.feedback.prepare()
                 }
                 .onEnded { _  in
                     if abs(self.offset.width) > 100 {
+                        if self.offset.width > 0 {
+                            self.feedback.notificationOccurred(.success)
+                        } else {
+                            self.feedback.notificationOccurred(.error)
+                        }
                         self.removal?()
                     } else {
                         self.offset = .zero
