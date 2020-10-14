d710085be4cf08cfa44e7092f469ca2da4cb9535
QDSG <piftao1127@163.com>
2020-10-14 16:15:49 +0800

    对齐和对齐指南

--- SwiftUI/ControllingUIFlow/Project-18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ SwiftUI/ControllingUIFlow/Project-18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,12 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        Text("Hello, world!")
-////            .padding(20)
-////            .padding()
-//            .background(Color.red)
-//            .padding()
-        Color.red
+//        Text("富强、民主、和谐")
+//            .frame(width: 300, height: 300, alignment: .topLeading)
+        
+//        HStack(alignment: .lastTextBaseline/*.bottom*/) {
+//            Text("Live")
+//                .font(.caption)
+//            Text("long")
+//            Text("and")
+//                .font(.title)
+//            Text("prosper")
+//                .font(.largeTitle)
+//        }
+        
+//        VStack(alignment: .leading) {
+//            Text("Hello, World!")
+//            Text("This is a longer line of text.")
+//        }
+//        .background(Color.red)
+//        .frame(width: 400, height: 400)
+//        .background(Color.blue)
+        
+//        VStack(alignment: .leading) {
+//            Text("Hello, World!")
+//                .alignmentGuide(.leading, computeValue: { dimension in
+////                    dimension[.leading]
+//                    dimension[.trailing]
+//                })
+//            Text("This is a longer line of text.")
+//        }
+        
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number: \(position)")
+                    .alignmentGuide(.leading, computeValue: { dimension in
+                        CGFloat(position) * -10
+                    })
+            }
+        }
+        .background(Color.red)
+        .frame(width: 400, height: 400)
+        .background(Color.blue)
     }
 }
 
