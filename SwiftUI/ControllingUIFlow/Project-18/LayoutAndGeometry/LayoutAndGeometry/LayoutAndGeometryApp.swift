15e1d192c3f94cc1abefed044cbabed231097842
QDSG <piftao1127@163.com>
2020-10-14 14:55:07 +0800

    init

---
+++ SwiftUI/ControllingUIFlow/Project-18/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by QDSG on 2020/10/14.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
