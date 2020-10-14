51e8118a1355e14ba74ebfa924e021ff14c4281e
QDSG <piftao1127@163.com>
2020-10-14 15:34:49 +0800

    SwiftUI中布局的工作方式

--- SwiftUI/ControllingUIFlow/Project-18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ SwiftUI/ControllingUIFlow/Project-18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Text("Hello, world!")
+////            .padding(20)
+////            .padding()
+//            .background(Color.red)
+//            .padding()
+        Color.red
     }
 }
 
