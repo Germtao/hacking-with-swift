b20f9215a1cb75aedcb83118bd32c7c0ce0230dd
QDSG <piftao1127@163.com>
2020-10-15 15:50:51 +0800

    建立项目的主要列表

--- SwiftUI/Project-19/SnowSeeker/SnowSeeker/ContentView.swift
+++ SwiftUI/Project-19/SnowSeeker/SnowSeeker/ContentView.swift
@@ -9,10 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let resorts = Resort.allResorts
+    
     var body: some View {
-//        SimpleTapAlertView()
-        
-        SimpleGroupContentView()
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink(
+                    destination: Text(resort.name),
+                    label: {
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(
+                                RoundedRectangle(cornerRadius: 5)
+                            )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(Color.black, lineWidth: 1.0)
+                            )
+                        
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                )
+            }
+            .navigationBarTitle("Resorts")
+        }
     }
 }
 
